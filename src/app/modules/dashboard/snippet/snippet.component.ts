29d3cc027121f90d95afb7f3ffd4a861192d88a1
onucsecu2 <onucsecu@gmail.com>
2023-01-14 20:06:47 +0900

    ui: add design snippets show page

--- src/app/modules/dashboard/snippet/snippet.component.ts
+++ src/app/modules/dashboard/snippet/snippet.component.ts
@@ -1,29 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '../../../services/auth/auth.service';
 import {RestService} from "../../../services/rest/rest.service";
 import {RestRoute} from "../../../services/rest/rest-route";
-import {map} from "rxjs";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './snippet.component.html',
   styleUrls: ['./snippet.component.css'],
 })
 export class SnippetComponent implements OnInit {
-  snippets: any[] = [];
+  snippets: any;
   constructor(
-    private authService: AuthService,
     private _rest: RestService,
-    private actRoute: ActivatedRoute
-  ) {
-    let id = this.actRoute.snapshot.paramMap.get('id');
-    let api = _rest.url(RestRoute.SNIPPETS);
-      _rest.get(api).pipe(
-        map((res) => {
-                this.snippets.push(res);
-        })
-      )
-    console.log(this.snippets)
+  ) { }
+  ngOnInit() {
+    let api = this._rest.url(RestRoute.SNIPPETS);
+    this._rest.get(api).subscribe({
+      next: value => {
+        this.snippets = value;
+      },
+      error: err => {
+        //No-op
+      }
+    })
   }
-  ngOnInit() {}
 }
