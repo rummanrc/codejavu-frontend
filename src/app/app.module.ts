4cfe9aec7e88a5b2be1ebda4977202d72892d2cf
onucsecu2 <onucsecu@gmail.com>
2023-01-20 00:57:48 +0900

    refactor: remove extend of HttpClient from RestService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RestService } from "./services/rest/rest.service";
@@ -34,13 +34,8 @@ import { RouteService } from "./services/route/route.service";
       multi: true },
     {
       provide: RestService,
-      useFactory: restServiceFactory,
-      deps: [HttpHandler]
     }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export function restServiceFactory(httpHandler: HttpHandler): RestService{
-  return new RestService(httpHandler);
-}
