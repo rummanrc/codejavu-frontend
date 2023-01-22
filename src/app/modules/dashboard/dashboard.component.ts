77da516bc6d9ac63e095eca0ae1b91f08d766747
onucsecu2 <onucsecu@gmail.com>
2023-01-22 15:20:10 +0900

    style: detach navbar component and add to the dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {route} from "../../constants";
+import {AuthService} from "../../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
   constructor() { }
 }
