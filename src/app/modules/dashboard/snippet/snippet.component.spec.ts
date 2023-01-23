370bfa3b506cba68bbd4ad426e1bbfe697b645ab
onucsecu2 <onucsecu@gmail.com>
2023-01-23 21:09:50 +0900

    test: add snippet component unit test

--- src/app/modules/dashboard/snippet/snippet.component.spec.ts
+++ src/app/modules/dashboard/snippet/snippet.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SnippetComponent } from './snippet.component';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {RestService} from "../../../services/rest/rest.service";
+import {AuthService} from "../../../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 describe('SnippetComponent', () => {
   let component: SnippetComponent;
   let fixture: ComponentFixture<SnippetComponent>;
-
+  let auth: AuthService;
+  let httpMock: HttpTestingController;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SnippetComponent ]
+      imports: [HttpClientTestingModule],
+      providers: [
+        RestService,
+        { provide: Router, useClass: class {
+            navigate = jasmine.createSpy("navigate").and.resolveTo(true);
+          }}
+      ],
+      declarations: [SnippetComponent]
     })
     .compileComponents();
   });
@@ -16,6 +28,8 @@ describe('SnippetComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SnippetComponent);
     component = fixture.componentInstance;
+    auth = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
