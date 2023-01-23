8e671c5c9b4befbc952603aca273f01a1c826f33
onucsecu2 <onucsecu@gmail.com>
2023-01-23 22:35:03 +0900

    test: add snippet component unit test

--- src/app/modules/dashboard/snippet/snippet.component.spec.ts
+++ src/app/modules/dashboard/snippet/snippet.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
 import { SnippetComponent } from './snippet.component';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
@@ -33,7 +33,134 @@ describe('SnippetComponent', () => {
     fixture.detectChanges();
   });
 
+  const languageList = [
+    {
+      "id": 1,
+      "name": "java"
+    },
+    {
+      "id": 2,
+      "name": "php"
+    },
+    {
+      "id": 3,
+      "name": "c++"
+    },
+    {
+      "id": 4,
+      "name": "javascript"
+    }
+  ];
+  const tagList = [
+    {
+      "id": 1,
+      "name": "general"
+    },
+    {
+      "id": 2,
+      "name": "world"
+    },
+    {
+      "id": 3,
+      "name": "auth"
+    }
+  ];
+  const snipets = [
+    {
+      "id": 2,
+      "title": "title 1",
+      "language": "php",
+      "tags": []
+    },
+    {
+      "id": 1,
+      "title": "title 2",
+      "language": "java",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "title": "title 3",
+      "language": "c++",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        }
+      ]
+    },
+    {
+      "id": 4,
+      "title": "title 4",
+      "language": "php",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        }
+      ]
+    },
+    {
+      "id": 5,
+      "title": "title 5",
+      "language": "php",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        },
+        {
+          "id": 2,
+          "name": "world"
+        },
+        {
+          "id": 3,
+          "name": "auth"
+        }
+      ]
+    },
+    {
+      "id": 6,
+      "title": "title 6",
+      "language": "php",
+      "tags": [
+        {
+          "id": 2,
+          "name": "world"
+        }
+      ]
+    }
+  ]
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show list of the snippets', fakeAsync(() => {
+    const spyLoadSnippet = spyOn<any>(component, 'loadSnippetList');
+    const spyLoadLanguage = spyOn<any>(component, 'loadLanguageList');
+    const spyLoadTag = spyOn<any>(component, 'loadTagList');
+
+    spyOnProperty(component, "snippets", "get").and.returnValue(snipets);
+    spyOnProperty(component, 'languages',"get").and.returnValue(languageList);
+    spyOnProperty(component, 'tags', "get").and.returnValue(tagList);
+
+    component.ngOnInit();
+
+    expect(spyLoadSnippet).toHaveBeenCalled();
+    expect(spyLoadLanguage).toHaveBeenCalled();
+    expect(spyLoadTag).toHaveBeenCalled();
+
+    fixture.detectChanges();
+
+    const el_snippets = fixture.nativeElement.querySelectorAll("table.table>tbody>tr");
+    expect(el_snippets.length).toEqual(6);
+  }));
+
+
 });
