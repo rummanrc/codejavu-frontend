d0a882a62ef9ecb514bae3d5894ec6d5db3ed249
onucsecu2 <onucsecu@gmail.com>
2023-01-24 01:29:12 +0900

    test: add snippet component unit test

--- src/app/modules/dashboard/snippet/snippet.component.spec.ts
+++ src/app/modules/dashboard/snippet/snippet.component.spec.ts
@@ -1,10 +1,12 @@
-import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { SnippetComponent } from './snippet.component';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {RestService} from "../../../services/rest/rest.service";
 import {AuthService} from "../../../services/auth/auth.service";
 import {Router} from "@angular/router";
+import {of} from "rxjs";
+import {DashboardModule} from "../dashboard.module";
 
 describe('SnippetComponent', () => {
   let component: SnippetComponent;
@@ -13,7 +15,7 @@ describe('SnippetComponent', () => {
   let httpMock: HttpTestingController;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, DashboardModule],
       providers: [
         RestService,
         { provide: Router, useClass: class {
@@ -33,6 +35,21 @@ describe('SnippetComponent', () => {
     fixture.detectChanges();
   });
 
+  const snippet = {
+    "id": 1,
+    "title": "title 1",
+    "language": "java",
+    "tags": [
+      {
+        "id": 1,
+        "name": "general"
+      }
+    ],
+    "snippet": "<?php echo \"jjjjjjjjjjjjjj\">",
+    "urls": [
+      "asdsd"
+    ]
+  };
   const languageList = [
     {
       "id": 1,
@@ -135,7 +152,9 @@ describe('SnippetComponent', () => {
         }
       ]
     }
-  ]
+  ];
+  let showModal: boolean = false;
+  let showEditModal: boolean = false;
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -155,12 +174,52 @@ describe('SnippetComponent', () => {
     expect(spyLoadSnippet).toHaveBeenCalled();
     expect(spyLoadLanguage).toHaveBeenCalled();
     expect(spyLoadTag).toHaveBeenCalled();
-
     fixture.detectChanges();
+
+    const el_snippet_show_dialog = fixture.nativeElement.querySelector("app-snippet-show-dialog");
+    expect(el_snippet_show_dialog.getAttribute("ng-reflect-modal-active")).toBe('false');
+
+    const el_snippet_edit_dialog = fixture.nativeElement.querySelector("app-snippet-create-edit-dialog");
+    expect(el_snippet_edit_dialog.getAttribute("ng-reflect-modal-active")).toBe('false');
 
     const el_snippets = fixture.nativeElement.querySelectorAll("table.table>tbody>tr");
     expect(el_snippets.length).toEqual(6);
   }));
 
+  it('should show the snippet', fakeAsync(() => {
+    spyOn<any>(component, 'loadSnippetList');
+    spyOn<any>(component, 'loadLanguageList');
+    spyOn<any>(component, 'loadTagList');
+    spyOn<any>(component, 'showCodeSnippet').and.callFake(() => {
+      showModal = true;
+    });
+
+    spyOnProperty(component, "snippets", "get").and.returnValue(snipets);
+    spyOnProperty(component, 'languages',"get").and.returnValue(languageList);
+    spyOnProperty(component, 'tags', "get").and.returnValue(tagList);
+    spyOnProperty(component, 'snippet', "get").and.returnValue(snippet);
+    spyOnProperty(component, 'isModalCreateEditActive', "get").and.returnValue(false);
+    spyOnProperty(component, 'isModalActive', "get").and.callFake(() => {
+      return showModal;
+    });
+    spyOn(component, 'closeSnippetModal').and.callFake(() => {
+      showModal = false;
+    });
+    spyOn(component, 'showAddEditCodeSnippet');
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const el_snippet = fixture.nativeElement.querySelector("table.table>tbody>tr:first-child>td:first-child");
+    expect(el_snippet).toBeTruthy();
+    el_snippet.dispatchEvent(new Event('click'));
+    tick();
+    fixture.detectChanges();
+    const el_snippet_show_dialog = fixture.nativeElement.querySelector("app-snippet-show-dialog");
+    expect(el_snippet_show_dialog.getAttribute("ng-reflect-modal-active")).toBe('true');
+
+    const el_snippet_edit_dialog = fixture.nativeElement.querySelector("app-snippet-create-edit-dialog");
+    expect(el_snippet_edit_dialog.getAttribute("ng-reflect-modal-active")).toBe('false');
+  }));
 
 });
