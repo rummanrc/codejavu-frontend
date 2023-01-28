fa91c681b6214a9120243a8c46a90ada4ceda5e2
Onu <onucsecu@gmail.com>
2023-01-28 11:38:59 +0900

    refactor: fix router-outlet error console log in unit test

--- src/app/modules/login/login.component.spec.ts
+++ src/app/modules/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +9,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
